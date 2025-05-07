2e6b29634f71ca35c1c7e67b2b25142005870264
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-05-07 16:43:00 +0530

    add mongoDB atlas in .env file

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+let MONGODB_URI = process.env.MONGODB_URI;
+let MONGODB_ATLAS = process.env.MONGODB_ATLAS;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(MONGODB_ATLAS);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
